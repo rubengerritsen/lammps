5b1df9acbda06753d7b85cbda526c8136d9839be
rubengerritsen <r.h.j.gerritsen@gmail.com>
2021-07-13 14:47:11 +0200

    with new options orca

--- QMMM/frame_798500/job_1_ACETONE_0:s1/system.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir -p /tmp/qmpackage/id
-/opt/orca-4.2.1/orca system.inp > system.log
-/opt/orca-4.2.1/orca_2mkl system -molden
--- VACCUUM/readme.md
+++
@@ -1,6 +0,0 @@
-# How to do a vaccuum calculation
-
-
-```bash
-xtp_tools -e dftgwbse -c job_name=acetoneOpt -o dftgwbse.xml -t 8
-```
